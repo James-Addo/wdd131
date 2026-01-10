934b175ab09ef893a8c556088803888ce46ad25b
James-Addo <jaddo6@byupathway.edu>
2026-01-10 09:52:49 +0000

    Updated

---
+++ week02/scripts/bomscript.js
@@ -0,0 +1,24 @@
+const input = document.querySelector(`#favchap`);
+const button = document.querySelector(`button`);
+const list = document.querySelector(`ul`);
+
+const li = document.createElement(`li`);
+
+const deleteButton = document.createElement(`button`);
+
+li.textContent = input.value;
+deleteButton.textContent = 'X';
+li.append(deleteButton);
+
+list.append(`li`);
+
+button.addEventListener(`click`, function () {
+    if (input.value.trim() !== '') { <button aria-label="Close" id="close-button">X</button> }
+});
+
+
+deleteButton.addEventListener('click', function () {
+    list.removeChild(li);
+    input.value = '';
+    input.focus();
+});
